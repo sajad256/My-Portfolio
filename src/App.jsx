8c9c2d2d4dc599ea95476031f01dc25749ec8fcd
Sajad Danish <Sajaddanish256@gmail.com>
2025-11-23 18:57:05 +0430

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Particles from "./Components/Hero/Particles";
 import Lightning from "./Lightning";
 import Iridescence from "./Iridescence"; // import Iridescence
 import { useState, useEffect } from "react";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function App() {
   const [themeMode, setThemeMode] = useState(() => {
@@ -61,6 +62,51 @@ export default function App() {
           ) : (
             <Particles themeMode={themeMode} />
           )}
+        </div>
+
+        {/* Social Icons fixed right */}
+        <div
+          style={{
+            position: "fixed",
+            right: 20,
+            top: "77%",
+            transform: "translateY(-50%)",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "16px",
+            zIndex: 50,
+          }}
+        >
+          <a
+            href="https://www.linkedin.com/feed/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#0077B5" }}
+          >
+            <FaLinkedin size={20} />
+          </a>
+          <a
+            href="https://github.com/sajad256"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#333" }}
+          >
+            <FaGithub size={20} />
+          </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: "#C13584" }}
+          >
+            <FaInstagram size={20} />
+          </a>
+          <div
+            className={`w-[2px] h-[200px] ${
+              themeMode === "dark" ? "bg-white" : "bg-black"
+            }`}
+          ></div>
         </div>
 
         {/* Main content */}
